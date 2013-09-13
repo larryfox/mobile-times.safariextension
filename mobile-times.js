c81210c9b41328680b7caeadee49bfb48e9a6334
Larry Fox <larrywfox@gmail.com>
2013-09-13 11:45:28 -0400

    Check for match

--- mobile-times.js
+++ mobile-times.js
@@ -1,7 +1,7 @@
 var re = /^https?:\/\/(\w+)(?:\.\w+)?\.nytimes\.com\/\d{4}\/\d{2}\/\d{2}/i
 var href = window.location.href
 var match = href.match(re)
-if (match[1]) {
+if (match && match[1]) {
   var subdomain = match[1]
   if (subdomain === 'www') {
     href = href.replace('www.nytimes.com', 'mobile.nytimes.com')

9920109926016e4cc0bb3efcb5dba1ddceca0ef0
Larry Fox <larrywfox@gmail.com>
2013-09-13 11:40:09 -0400

    Fix blogs

--- mobile-times.js
+++ mobile-times.js
@@ -1,5 +1,13 @@
+var re = /^https?:\/\/(\w+)(?:\.\w+)?\.nytimes\.com\/\d{4}\/\d{2}\/\d{2}/i
 var href = window.location.href
-if (href.match(/^https?:\/\/www\.nytimes\.com\/\d{4}\/\d{2}\/\d{2}/)) {
-  href = href.replace('www.nytimes.com','mobile.nytimes.com')
+var match = href.match(re)
+if (match[1]) {
+  var subdomain = match[1]
+  if (subdomain === 'www') {
+    href = href.replace('www.nytimes.com', 'mobile.nytimes.com')
+  } else {
+    var new_domain = 'mobile.nytimes.com/blogs/'+subdomain
+    href = href.replace(/(\w+)(?:\.\w+)?\.nytimes\.com/i, new_domain)
+  }
   window.location.href = href
 }
